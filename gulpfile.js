26cbb80ddcbd7102550086c89f385f957073cadc
Anna Volodskikh <volodskih@gmail.com>
2015-10-26 06:09:58 -0400

    added deafault task

--- gulpfile.js
+++ gulpfile.js
@@ -38,3 +38,11 @@ var gulp = require('gulp'),
 			 .on('error', gutil.log)
 			.pipe(gulp.dest('builds/development/css'))
 	});
+
+	gulp.task('watch', function(){
+		gulp.watch(coffeeSources, ['coffee']);
+		gulp.watch(jsSources, ['js']);
+		gulp.watch('components/sass/*.scss', ['compass']);
+	})
+
+	gulp.task('default',['coffee','js','compass','watch'])
