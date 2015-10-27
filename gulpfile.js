f894e31e79f24d2dab350ccc9ff9cda8b507edf0
Anna Volodskikh <volodskih@gmail.com>
2015-10-27 17:59:16 -0400

    Added live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
+	connect = require('gulp-connect'),
 	concat = require('gulp-concat');
 
 
@@ -26,6 +27,7 @@ var gulp = require('gulp'),
 			.pipe(concat('script.js'))
 			.pipe(browserify())
 			.pipe(gulp.dest('builds/development/js'))
+			.pipe(connect.reload())
 	});
 
 	gulp.task('compass', function(){
@@ -37,6 +39,7 @@ var gulp = require('gulp'),
 			}))
 			 .on('error', gutil.log)
 			.pipe(gulp.dest('builds/development/css'))
+			.pipe(connect.reload())
 	});
 
 	gulp.task('watch', function(){
@@ -45,4 +48,11 @@ var gulp = require('gulp'),
 		gulp.watch('components/sass/*.scss', ['compass']);
 	})
 
-	gulp.task('default',['coffee','js','compass','watch'])
+	gulp.task('connect', function(){
+		connect.server({
+			root: 'builds/development/',
+			livereload: true
+		});
+	});
+
+	gulp.task('default',['coffee','js','compass', 'connect', 'watch']);
